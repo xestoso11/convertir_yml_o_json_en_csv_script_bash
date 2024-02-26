de813400ddc7a0224d5ee0846a19af724cbc12c8
xestoso11 <84078701+xestoso11@users.noreply.github.com>
2024-02-26 17:03:34 +0100

    Update yml_csv.sh

--- yml_csv.sh
+++ yml_csv.sh
@@ -5,3 +5,11 @@ if[ $? -ne 0 ];then
     apt-get update
     apt -y install yq
 fi
+
+yq ".[].nombre" ejemplo.json > nombre.tmp
+yq -r ".[].edad" ejemplo.json > edad.tmp
+
+echo "nombre;edad" > persona.csv
+paste -d ";" nombre.tmp edad.tmp >> persona.csv
+
+rm nombre.tmp edad.tmp
